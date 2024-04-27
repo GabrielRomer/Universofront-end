b94089a763dc7d820a728c627688618cf772f89b
Gabriel Romer <gabrielromer92@gmail.com>
2024-04-27 17:38:41 -0300

    Descrição das alterações feitas

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://universofront-end-p84r.vercel.app/', // Defina o endereço base da sua API
+  baseURL: "https://universofront-end-p84r.vercel.app/", // Defina o endereço base da sua API
 });
 
 export default api;
