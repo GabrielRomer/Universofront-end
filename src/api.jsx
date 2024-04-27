e75d6c8176f7eb4f53263847ad4ee340251f175b
Gabriel Romer <gabrielromer92@gmail.com>
2024-04-27 20:56:38 -0300

    tentando resolver

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Usando a variável de ambiente para definir o baseURL,
+  baseURL: process.env.REACT_APP_API_URL, // Usando a variável de ambiente para definir o baseURL,
 });
 
 export default api;
