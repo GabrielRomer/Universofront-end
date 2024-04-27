9e8d01f482f10e0c8dd8d6ceebba6407d5c6bc6c
Gabriel Romer <gabrielromer92@gmail.com>
2024-04-27 20:45:18 -0300

    ainda nas rotas2

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./assets/Paginas/Home";
 import Contato from "./assets/Paginas/Contato";
 import Conteudo from "./assets/Paginas/Conteudo";
@@ -6,16 +6,13 @@ import Conteudo2 from "./assets/Paginas/Conteudo2";
 
 function Rotas() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/conteudo" element={<Conteudo />} />
-        <Route path="/conteudo2" element={<Conteudo2 />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="/contato" element={<Contato />} />
+      <Route path="/conteudo" element={<Conteudo />} />
+      <Route path="/conteudo2" element={<Conteudo2 />} />
+    </Routes>
   );
 }
 
 export default Rotas;
-   
